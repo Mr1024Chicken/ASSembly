e40d39da44664e1bb1011f7a60a925b1e53f8af2
kingko555 <kha.vo@mavs.uta.edu>
2016-11-07 15:01:35 -0600

    Add files via upload

--- HW_1.s
+++ HW_1.s
@@ -10,7 +10,7 @@ Programming Assignment #1
 	.asciz "%d\n"
 
 main:
-	BL _prompt
+	@BL _prompt
 
     	BL _getchar             @get first number store in R4
     	MOV R4,[R0]
@@ -81,13 +81,13 @@ _getchar:
     	MOV PC, LR              @ return
 
 
-_prompt:
-    	MOV R7, #4              @ write syscall, 4 (R7 is an operation. 4 iswrite, one of R7 funct).
-    	MOV R0, #1              @ output stream to monitor, 1  ( which stream to go to ex. #1).
-    	MOV R2, #23             @ print string length
-    	LDR R1, =prompt_str     @ string at label prompt_str: (R1 system call to print)
-    	SWI 0                   @ execute syscall
-    	MOV PC, LR              @ return (back to main).
+@_prompt:
+@    	MOV R7, #4              @ write syscall, 4 (R7 is an operation. 4 iswrite, one of R7 funct).
+@    	MOV R0, #1              @ output stream to monitor, 1  ( which stream to go to ex. #1).
+@    	MOV R2, #23             @ print string length
+@   	LDR R1, =prompt_str     @ string at label prompt_str: (R1 system call to print)
+@    	SWI 0                   @ execute syscall
+@    	MOV PC, LR              @ return (back to main).
 
 
 
