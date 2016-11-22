a33edb5a338fadd2bb220ed8e233c4db6d6ff7bd
Kha Vo <pi@raspberry.com>
2016-11-22 13:31:37 -0600

    working mod

--- try.s
+++ try.s
@@ -7,7 +7,7 @@ Programming Assignment #1
 .func main
 
 .Lout:
-	.asciz "%d\n"
+	.asciz "%d mod %d = %d\n"
 
 main:
 	BL _prompt
@@ -15,14 +15,22 @@ main:
     	BL _scanf             @get first number store in R4
     	MOV R4,R0
 
-	BL _getchar             @get operand store in R5
-    	MOV R3,R0
+	@BL _getchar             @get operand store in R5
+    	@MOV R3,R0
 
 	BL _scanf             @get second number store in R3
     	MOV R5,R0
+	mov R7, R4
+	mov R8, R5
 	
 	BL _mod_unsigned
-	
+	mov R3, R0
+	mov R1, R7
+	mov R2, R8
+	ldr R0, =.Lout
+	bl printf
+	bl main
+
 _mod_unsigned:
     cmp R5, R4          @ check to see if R1 >= R2
     MOVHS R0, R4        @ swap R1 and R2 if R2 > R1
@@ -37,60 +45,15 @@ _mod_unsigned:
         CMP R4, R5      @ check for loop termination
         BHS _modloop    @ continue loop if R1 >= R2
     MOV R0, R4          @ move remainder to R0
-    @MOV PC, LR          @ return
+    MOV PC, LR          @ return
 
-_add:
-	add R4, R4, R3
-	mov R1,R4
-	ldr R0, =.Lout
-	bl printf
-	b main
-
-_sub:
-	sub R4,R4,R3
-	mov R1,R4
-	ldr R0, =.Lout
-	bl printf
-	b main
-_mul:
-	mul R4,R3,R4
-	mov R1,R4
-	ldr R0, =.Lout
-	bl printf
-	b main
-_max:
-	CMP R4,R3
-	BGT _greater
-	BLT _less
-
-_greater: 
-	mov R1,R4
-	ldr R0, =.Lout
-	bl printf
-	b main
-
-_less:
-	mov R1,R3
-	ldr R0, =.Lout
-	bl printf
-	b main
-	
     
-_getchar:
-    	MOV R7, #3              @ write syscall, 3 (3 is getting something).
-    	MOV R0, #0              @ input stream from monitor, 0 
-    	MOV R2, #1              @ read a single character
-    	LDR R1, =read_char      @ store the character in data memory
-    	SWI 0                   @ execute the system call
-    	LDR R0, [R1]            @ move the character to the return register
-    	AND R0, #0xFF          @ mask out all but the lowest 8 bits   ( mask using 0xFF= 0000 0000 0000 0000 0000 0000 1111 1111) taking only the last 8 bit
-    	MOV PC, LR              @ return
 
 
 _prompt:
 	MOV R7, #4              @ write syscall, 4 (R7 is an operation. 4 iswrite, one of R7 funct).
     	MOV R0, #1              @ output stream to monitor, 1  ( which stream to go to ex. #1).
-    	MOV R2, #11             @ print string length
+    	MOV R2, #6             @ print string length
    	LDR R1, =prompt_str     @ string at label prompt_str: (R1 system call to print)
     	SWI 0                   @ execute syscall
     	MOV PC, LR              @ return (back to main).
@@ -107,7 +70,7 @@ _scanf:
 
 .data 
 
-prompt_str:     .ascii	"Calculator:\n "
+prompt_str:     .ascii	"Mode:\n"
 read_char:	.ascii	" "
 format_str:	.ascii	"%d"	
 .end 
